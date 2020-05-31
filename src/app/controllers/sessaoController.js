a3911eb310d7ee99de60911b2f9e2669209fd1b4
Filubil <denison.shenry@gmail.com>
2020-05-30 22:09:43 -0300

    Teste heroku

--- src/app/controllers/sessaoController.js
+++ src/app/controllers/sessaoController.js
@@ -6,47 +6,51 @@ import Arquivos from '../models/arquivos';
 
 class SessaoController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required(),
-      senha: Yup.string().required(),
-    });
-    if (!(await schema.isValid(req.body))) {
-      return res.status(401).json({ erro: 'Dados inválidos!' });
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required(),
+        senha: Yup.string().required(),
+      });
+      if (!(await schema.isValid(req.body))) {
+        return res.status(401).json({ erro: 'Dados inválidos!' });
+      }
+      const { email, senha } = req.body;
+
+      const usuario = await Usuarios.findOne({
+        where: { email },
+        include: [
+          {
+            model: Arquivos,
+            as: 'avatar',
+            attributes: ['id', 'arquivo', 'url'],
+          },
+        ],
+      });
+
+      if (!usuario) {
+        return res.status(401).json({ erro: 'E-mail inválido!' });
+      }
+      if (!(await usuario.checkPassword(senha))) {
+        return res.status(401).json({ erro: 'Senha inválida!' });
+      }
+
+      const { id, nome, avatar } = usuario;
+
+      return res.json({
+        mensagem: 'Sessão Criada',
+        id,
+        nome,
+        email,
+        avatar,
+        token: jwt.sign({ id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      });
+    } catch (error) {
+      console.log('error ao tentar criar sessão', error);
     }
-    const { email, senha } = req.body;
-
-    const usuario = await Usuarios.findOne({
-      where: { email },
-      include: [
-        {
-          model: Arquivos,
-          as: 'avatar',
-          attributes: ['id', 'arquivo', 'url'],
-        },
-      ],
-    });
-
-    if (!usuario) {
-      return res.status(401).json({ erro: 'E-mail inválido!' });
-    }
-    if (!(await usuario.checkPassword(senha))) {
-      return res.status(401).json({ erro: 'Senha inválida!' });
-    }
-
-    const { id, nome, avatar } = usuario;
-
-    return res.json({
-      mensagem: 'Sessão Criada',
-      id,
-      nome,
-      email,
-      avatar,
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
   }
 }
 
